6b3fbe6263bb90b0c318c9286d845728eb3efd1e
Synapses Developer <dev@onlinesynapses.com>
2026-08-01 14:01:31 +0500

    Add Hilt-Work deps for @HiltWorker in backup and restore modules

--- features/restore/build.gradle.kts
+++ features/restore/build.gradle.kts
@@ -43,6 +43,10 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-compiler:2.48")
     
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
     testImplementation("junit:junit:4.13.2")
