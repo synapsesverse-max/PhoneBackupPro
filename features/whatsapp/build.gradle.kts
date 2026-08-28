842e73cd268798c23acb5b077771feaf77bf3957
synapsesverse-max <2.93341635e+08+synapsesverse-max@users.noreply.github.com>
2026-08-28 02:26:18 +0000

    test: add AndroidX instrumentation runner

--- features/whatsapp/build.gradle.kts
+++ features/whatsapp/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 }
